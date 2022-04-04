e1c4d8c75227ed99728fb794a9f8f0f975960a5e
jakjan95 <jakubjanusz95@gmail.com>
2022-04-04 21:08:02 +0200

    Data sharing module exercises done

--- module2/exercises/02_threads_in_collection.cpp
+++ module2/exercises/02_threads_in_collection.cpp
@@ -1,20 +1,49 @@
+#include <sstream>
 #include <vector>
 #include <thread>
 #include <chrono>
 #include <iostream>
-using namespace std;
+#include <mutex>
 
+using namespace std::chrono_literals;
 // Secure your code so that each thread can safely enter its full text into the stream.
 
+/*
+Solution 1 using global mutex, not safe
+std::mutex m;
 void do_work(int id) {
     this_thread::sleep_for(100ms);
+    m.lock();
     cout << "Thread [" << id << "]: " << "Job done!" << endl;
+    m.unlock();
+}
+*/
+
+//Solution 2 using RAII lock_gurad
+void do_work(int id, std::mutex& m)
+{
+    std::this_thread::sleep_for(100ms);
+    std::lock_guard<std::mutex> lock(m);
+    std::cout << "Thread [" << id << "]: "
+              << "Job done!" << std::endl;
 }
 
+/*Solution 3 fastes:
+void do_work(int id)
+{
+    std::this_thread::sleep_for(100ms);
+    std::stringstream ss;
+    ss << "Thread [" << id << "]: "
+       << "Job done!" << std::endl;
+    std::cout << ss.rdbuf();
+}
+*/
+
 int main() {
-    vector<thread> threads;
+    std::mutex m;
+    std::vector<std::thread> threads;
     for (int i = 0; i < 20; i++) {
-        threads.emplace_back(thread(do_work, i));
+        threads.emplace_back(std::thread(do_work, i, std::ref(m)));
     }
     for (auto && t : threads) {
         t.join();
--- module2/exercises/03_shared_mutex.cpp
+++ module2/exercises/03_shared_mutex.cpp
@@ -1,11 +1,17 @@
+#include <mutex>
 #include <vector>
 #include <iostream>
 #include <thread>
+#include <shared_mutex>
+
 using namespace std;
 
 // Use shared and / or regular locks
 // Use the appropriate lock managers
 // Compile with TSan
+
+std::shared_mutex sharedMutexNumbers;
+std::mutex countMutex;
 
 vector<int> numbers = {};
 
@@ -14,15 +20,21 @@ int getNextValue() {
     return i+=10;
 }
 
-void read(int index) {
+void read(int index)
+{
     for (int i = 0; i < 10; i++) {
+        std::shared_lock<std::shared_mutex> lock(sharedMutexNumbers);
         int value = numbers[index];
+        lock.unlock();
+        std::lock_guard<std::mutex> countLock(countMutex);
         cout << value << " ";
     }
 }
 
-void write() {
+void write()
+{
     for (int i = 0; i < 3; i++) {
+        std::lock_guard<std::shared_mutex> lock(sharedMutexNumbers);
         int newValue = getNextValue();
         numbers.emplace_back(newValue);
     }
@@ -34,9 +46,13 @@ int main() {
         writers.emplace_back(write);
 
     cout << "Writers produced: ";
-    for(const auto & n : numbers)
+
+    std::shared_lock<std::shared_mutex> lock(sharedMutexNumbers);
+    for (const auto& n : numbers) {
         cout << n << " ";
+    }
     cout << endl;
+    lock.unlock();
 
     cout << "Readers consumed: ";
     vector<thread> readers;
