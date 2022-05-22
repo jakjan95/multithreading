b0e7d838c0218b59399b1cb9443db12903567ce9
jakjan95 <jakubjanusz95@gmail.com>
2022-05-22 15:13:21 +0200

    ping pong exercise done

--- topics/04_condition_variable/exercises/03a_ping_pong.cpp
+++ topics/04_condition_variable/exercises/03a_ping_pong.cpp
@@ -1,30 +1,77 @@
-#include <iostream>
-#include <thread>
+#include <atomic>
 #include <chrono>
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <thread>
 using namespace std;
 
 class PingPong {
     int repetitions_;
+    std::atomic<bool> play_ { true };
+    bool isPingTurn_ { true };
+    std::condition_variable cv_;
+    std::mutex mtx_;
 
 public:
     PingPong(int repetitions)
         : repetitions_(repetitions)
-    {}
-
-    void ping() {
-        // TODO: implement me :)
+    {
     }
 
-    void pong() {
-        // TODO: implement me :)
+    void ping()
+    {
+        thread_local int localReps = 0;
+        while (localReps < repetitions_ && play_) {
+            std::unique_lock<std::mutex> lock(mtx_);
+            cv_.wait(lock, [&] { return isPingTurn_; });
+            std::cout << "ping " << localReps << '\n';
+            localReps++;
+            isPingTurn_ = false;
+            lock.unlock();
+            this_thread::sleep_for(500ms);
+        }
+
+        if (localReps >= repetitions_) {
+            std::cout << "Ping reached repetitions limit\n";
+        }
     }
 
-    void stop([[maybe_unused]] chrono::seconds timeout) {
-        // TODO: should stop execution after timeout
+    void pong()
+    {
+        thread_local int localReps = 0;
+        while (localReps < repetitions_ && play_) {
+            std::unique_lock<std::mutex> lock(mtx_);
+            cv_.wait(lock, [&] { return !isPingTurn_; });
+            std::cout << "pong " << localReps << '\n';
+            localReps++;
+            isPingTurn_ = true;
+            lock.unlock();
+            this_thread::sleep_for(500ms);
+        }
+
+        if (localReps >= repetitions_) {
+            std::cout << "Pong reached timout limit\n";
+            play_ = false;
+            cv_.notify_all();
+        }
+    }
+
+    void stop([[maybe_unused]] chrono::seconds timeout)
+    {
+        std::unique_lock<std::mutex> lock(mtx_);
+        //bug in wait_for causing double lock of a mutex
+        auto isTimeout = cv_.wait_for(lock, timeout, [&] { return !play_; });
+        if (!isTimeout) {
+            play_ = false;
+            std::cout << "Timeout\n";
+        }
+        cv_.notify_all();
     }
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     if (argc != 3) {
         return -1;
     }
